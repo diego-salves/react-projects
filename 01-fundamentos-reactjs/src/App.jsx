157d0e5870d89dedd05662dbc9021af110c2a4fc
diego-salves <diegosantos57@outlook.com.br>
2022-11-25 00:23:07 -0300

    created Post props

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -3,13 +3,14 @@ import { Post } from './Post'
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post 
+        author="Diego" 
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+      />
+      <Post 
+        author="José" 
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+      />
     </div>
   )
 }
