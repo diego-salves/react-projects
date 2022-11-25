1f2e25c3bfff46e3b888c47f53cc3b04e7b7d40e
diego-salves <diegosantos57@outlook.com.br>
2022-11-24 23:40:06 -0300

    starting project

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,37 +1,34 @@
-// JSX = JavaScript + XML
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-import { Header } from './components/Header';
-import {Post} from './Post';
+function App() {
+  const [count, setCount] = useState(0)
 
-import styles from './App.module.css';
-
-import './global.css';
-
-export function App() {
   return (
-    <div>
-      <Header />
-
-      <div className={styles.wrapper}>
-        <aside>
-          Sidebar
-        </aside>
-        <main>
-          <Post 
-            author="Diego Alves"
-            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, totam provident vitae dolore voluptatum error vero expedita nam quis officiis esse quaerat est, non cupiditate. Aliquam enim amet similique laboriosam!"
-          />
-
-          <Post 
-            author="Lux"
-            content="demácia!"
-          />
-        </main>
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
   )
 }
 
-
-
+export default App
