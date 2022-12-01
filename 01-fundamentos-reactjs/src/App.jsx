07ae24b41b3ae1bd49fe50c92c645eab117b8412
diego-salves <diegosantos57@outlook.com.br>
2022-11-30 23:33:32 -0300

    created global styles using css modules

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,10 +1,13 @@
-import { Post } from './Post'
+import { Header } from './components/Header';
+import { Post } from './Post';
 
-import './styles.css'
+import './global.css';
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post 
         author="Diego" 
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
