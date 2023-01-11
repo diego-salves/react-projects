1410ffb990e988e1c49f8cf8021c0a04cd5be03a
diego-salves <diegosantos57@outlook.com.br>
2023-01-11 00:06:29 -0300

    refactoring to css modules

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,13 +1,8 @@
-import { Header } from './components/Header';
 import { Post } from './Post';
-
-import './global.css';
 
 export function App() {
   return (
     <div>
-      <Header />
-
       <Post 
         author="Diego" 
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
