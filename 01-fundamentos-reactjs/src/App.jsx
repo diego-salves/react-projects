72bf0f0bb355c615b675d20776f1bb5ae4d549a9
diego-salves <diegosantos57@outlook.com.br>
2023-01-13 00:05:41 -0300

    addind global css

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,13 @@
 import { Post } from './Post';
 
+import './global.css';
+import { Header } from './components/Header';
+
 export function App() {
   return (
     <div>
+      <Header />
+      
       <Post 
         author="Diego" 
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
